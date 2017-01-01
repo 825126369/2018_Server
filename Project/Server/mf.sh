3a488d0b2bd94d668e73837d6cb117eb348db342
xuke <825126369@qq.com>
2017-01-01 20:54:47 +0800

    接近完美的makefile

--- Project/Server/mf.sh
+++ Project/Server/mf.sh
@@ -6,7 +6,7 @@ getallcpp()
 	do 
 		dir_or_file=$1"/"$element 
 		if [ -d $dir_or_file ];then 
-			getallcpp $dir_or_file
+			getallcpp $dir_or_file $2
 		elif [ -e $dir_or_file ]; then 
 			extention="${dir_or_file##*.}" 	
 			if [ -z $2 ];then
@@ -45,7 +45,7 @@ get_d()
 	getallcpp $rootdir $2
 	for x in $result_c; 
 	do 
-		result_d="$result_d $x.$1";
+		result_d="$result_d $outdir$x.$1";
 	done
 	result_d=$result_d
 }
@@ -54,27 +54,27 @@ get_o()
 	getallcpp $rootdir
 	for x in $result_c;
 	do 
-		result_o="$result_o $x.o"; 
+		result_o="$result_o $outdir$x.o"; 
 	done
 	result_o=$result_o
 }
 get_action()
 {
-	#echo '		$(gg) $(CFLAG) -o $@ $<' >>$1
-	echo $1
+	echo '		$(gg) $(CFLAG) -o $@ $<' >>$1
 }
 rootdir=$1
-if [ $2 = "c" ];then
+outdir=$2
+if [ $3 = "c" ];then
 	get_cpp $rootdir
 	echo $result_cpp
-elif [ $2 = "d" ];then
-	get_d $3 $4
+elif [ $3 = "d" ];then
+	get_d $4 $5
 	echo $result_d
-elif [ $2 = "o" ];then
+elif [ $3 = "o" ];then
 	get_o
 	echo $result_o
-elif [ $2 = "action" ];then
-	echo $3
+elif [ $3 = "action" ];then
+	get_action
 else
 	echo "Input Error! "
 	echo '$1 : RootDir'
